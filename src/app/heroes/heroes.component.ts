4089fa9a651ca8a862a60ff8a60c04d446bd2ef5
omry.hazut <omry.hazut@mail.huji.ac.il>
2019-05-14 15:20:56 +0300

    Message Component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,12 +16,11 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
   // Async
-  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes); 
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
